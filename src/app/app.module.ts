7becc502fa16342589754db0572fb4cb6b810482
Harsh Singh <2388220@cognizant.com>
2025-04-04 11:01:20 +0530

    changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseManagementComponent } from './course-management/course-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseManagementComponent
   ],
   imports: [
     BrowserModule,
