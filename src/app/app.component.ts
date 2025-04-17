b873d2b1b0c5b4537f790e689985d13e0b85aa62
Shivangi Chakraborty <2388044@cognizant.com>
2025-04-17 08:22:18 +0530

    [shivangi]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html', // Ensure this path is correct
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone:false
 })
 export class AppComponent {
   title = 'course-management-app';
